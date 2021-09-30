cbf66b86d5b3de46ba4377855f4204154cf80536
josephmfaulkner <josephmfaulkner@gmail.com>
2021-09-30 11:10:05 -0700

    Axios get messages from API

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -4,7 +4,7 @@ import { withStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
 
-import MessageService from "../services/MessageService";
+import MessageService from "../data/services/MessageService";
 
 import SimpleCard from "./components/SimpleCard";
 
@@ -16,17 +16,36 @@ const useStyles = theme => ({
   
 
 class MainPage extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            messages : []
+        };
+    }
+
+    componentDidMount() {
+        console.log("Mounted");
+        MessageService.getMessages()
+            .then( ( newMessages ) => { 
+                this.setState({
+                    messages : newMessages
+                })
+            })
+            .catch( (error) => {console.log(error); });
+    }
+
     render(){
         const { classes } = this.props; 
 
-        const messages = MessageService.getMessages();
+        
 
         return (
             <Container maxWidth="sm"> 
                 <Box my={4} className={classes.root}>
                     {
-                        messages.map((message, index) => {
-                            return ( <SimpleCard key={index} message={ message } /> );
+                        this.state.messages.map((message, index) => {
+                            return ( <SimpleCard key={message.id} message={ message.message } /> );
                         })
                     }
                 </Box>

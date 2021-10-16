fbe05e59508d02ce277e9a829ba06a99b1e80d26
josephmfaulkner <josephmfaulkner@gmail.com>
2021-10-15 20:24:38 -0700

    removed unused imports

--- frontend-react/src/MainTemplate.js
+++ frontend-react/src/MainTemplate.js
@@ -1,11 +1,5 @@
 import React from "react";
 
-import Container from '@material-ui/core/Container';
-import Box from '@material-ui/core/Box';
-import Link from '@material-ui/core/Link';
-
-
-import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
--- frontend-react/src/pages/components/SimpleCard.js
+++ frontend-react/src/pages/components/SimpleCard.js
@@ -26,7 +26,6 @@ const useStyles = makeStyles({
 
 export default function SimpleCard(props) {
   const classes = useStyles();
-  const bull = <span className={classes.bullet}>•</span>;
 
   return (
     <Card className={classes.root}>

ba36038b269d7f0248eba70e0f278794a8974ce5
Steven Hollingsworth <shollingsworth@barracuda.com>
2022-03-21 17:07:35 -0700

    Try 2 on loading model

--- frontend/src/components/model/upload.vue
+++ frontend/src/components/model/upload.vue
@@ -84,6 +84,7 @@
 require("@tensorflow/tfjs-backend-cpu")
 require("@tensorflow/tfjs-backend-webgl")
 import * as tf from "@tensorflow/tfjs"
+import name_map from "@/static/model/name_map.json"
 
 export default {
     data() {
@@ -102,7 +103,7 @@ export default {
              */
             uploads: [],
             expanded: [],
-            name_map: {},
+            name_map: name_map,
         }
     },
     computed: {
@@ -145,9 +146,7 @@ export default {
     fetch() {
         return new Promise(async (resolve, _reject) => {
             const path = `/model/model.json`
-            const name_map_file = `/model/name_map.json`
             this.model = await tf.loadGraphModel(path)
-            this.name_map = await (await fetch(name_map_file)).json()
             console.log("model loaded", this.name_map, this.model)
             resolve()
         })
